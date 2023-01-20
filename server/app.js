3a96c7645bf5e7e3fb52b68c6cca2ecdb726f260
EdwinIvanS <sedwin118@hotmail.com>
2023-01-20 09:29:21 -0500

    creando servidor

---
+++ server/app.js
@@ -0,0 +1,33 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use( ( req, res, next ) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin','Content-Type, Authorization');    
+    next();
+});
+
+app.use('/api', indexRouter);
+
+// init port
+app.listen(3001, ()=>{console.log('server on port 3001')})
+
+module.exports = app;
